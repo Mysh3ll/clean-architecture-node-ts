af9cdb3fe6b7e32db01f2711a783d97cfc8f8600
Mysh3ll <peacefulloijkn@gmail.com>
2020-08-17 18:00:13 +0200

    Add usecase UpdateTask

--- src/domain/models/Task.ts
+++ src/domain/models/Task.ts
@@ -10,6 +10,7 @@ export class Task {
     constructor(title: string = null, description: string = null) {
         this._title = title;
         this._description = description;
+        this._createdAt = moment();
     }
 
     get id(): number {
@@ -29,4 +30,27 @@ export class Task {
     }
 
 
+    get description(): string {
+        return this._description;
+    }
+
+    set description(value: string) {
+        this._description = value;
+    }
+
+    get createdAt(): moment.Moment {
+        return this._createdAt;
+    }
+
+    set createdAt(value: moment.Moment) {
+        this._createdAt = value;
+    }
+
+    get updatedAt(): moment.Moment {
+        return this._updatedAt;
+    }
+
+    set updatedAt(value: moment.Moment) {
+        this._updatedAt = value;
+    }
 }
